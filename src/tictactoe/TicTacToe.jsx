a0e59a0b29ea46b056bbb7e5b0cccb52e4649061
Niko <niko.lahnajarvi@gmail.com>
2023-04-18 19:33:50 +0300

    Tehtiin pelitoiminnallisuutta.

--- src/tictactoe/TicTacToe.jsx
+++ src/tictactoe/TicTacToe.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import './TicTacToe.css';
 import { Board } from "./Board";
+import { useState } from "react";
 
 export const TicTacToe = () => {
+    const initialBoard=['','','','','','','','',''];
+    const [gameState, setGameState] = useState(initialBoard);
+    const [isXTurn, setIsXturn] = useState(true)
+
+    const onSquareClick = (index) => {
+        let strings = Array.from(gameState)
+        if (strings[index] !== ''){
+            return;
+        }
+        strings[index] = isXTurn ? 'x' : 'o';
+        setGameState(strings);
+        setIsXturn(!isXTurn)
+    }
+
     return (
         <div>
-            <h1>Tic-Tac-Toe</h1>
-            <Board />
+            <div className="game">
+                <h1>Tic-Tac-Toe</h1>
+                <Board gameState={gameState} onSquareClick={onSquareClick}/>
+            </div>
         </div>
     );
 }
